49f55a5bbbdf76708ddc8791bb8d37e773cbf1de
ismael.hini@domissori.fr Hini ismael <titann2.15@outlook.com>
2022-12-16 12:06:28 +0100

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { HomeComponent } from './home/home.component';
 
@@ -17,8 +17,14 @@ const routes: Routes = [
 
 ];
 
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  anchorScrolling: 'enabled',
+  // ...any other options you'd like to use
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,routerOptions)],
   exports: [RouterModule,],
 })
 export class AppRoutingModule {}
