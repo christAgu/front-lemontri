178eb2b7f556efe71e89eb8b0d72667d28dcfdf2
christvit <ague26oth@gmail.com>
2022-12-16 10:26:09 +0100

    anim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
-import { LesaviezvousComponent } from './lesaviezvous/lesaviezvous.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from './modal/modal.component';
-import { MiddleComponent } from './middle/middle.component';
-import { FoundersComponent } from './founders/founders.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AcceuilComponent,
-    LesaviezvousComponent,
+
     ModalComponent,
-    MiddleComponent,
-    FoundersComponent,
+
+   
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { MiddleComponent } from './../middle/middle.component';
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
-    
+
   }
 
 
